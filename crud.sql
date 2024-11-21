f9bdde7e9de5a1dcba29bfc089ae163d100d382f
Anup Gautam <anupgautam430@gmail.com>
2024-11-21 20:47:30 +0545

    schema

---
+++ crud.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE crud;
+
+USE crud;
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    phone VARCHAR(15) NOT NULL,
+    position VARCHAR(50) NOT NULL,
+    image LONGBLOB NOT NULL
+);
